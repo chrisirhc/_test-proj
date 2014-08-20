94ac59038c5d69f98821a430878593d51393b521
Chris Chua <chris.sirhc@gmail.com>
2014-08-20 01:16:09 -0700

    Attempt to use shell script

---
+++ .travis/make-release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    openssl aes-256-cbc -K $encrypted_0e643f7c177c_key -iv $encrypted_0e643f7c177c_iv -in .travis/id_release.enc -out ~/.ssh/id_rsa -d
+    chmod 600 ~/.ssh/id_rsa
+    git clone --depth 1 git@github.com:chrisirhc/_test-proj-releases.git release
+    mv build/* release/
+    version=`git rev-parse --short HEAD`
+    cd release/
+    git add .
+    git config user.name "Bot"
+    git commit -m "$version (automatic)"
+    git push origin master
+fi
